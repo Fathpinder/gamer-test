40343aa456b1dd008a4f70d34758c40002e4751d
Mark <37812183+Fathpinder@users.noreply.github.com>
2022-09-28 12:03:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var apiKey = '94d797ec-57ec-4610-88ca-137b97537b2e';
+
+var platform = 'origin';
+var platformUser = '_M3RK109';
+
+fetch(
+	'https://public-api.tracker.gg/v2/apex/standard/profile/origin/_M3RK109',
+	{
+		method: 'GET',
+		headers: {
+			'TRN-Api-Key': '94d797ec-57ec-4610-88ca-137b97537b2e',
+			'Access-Control-Allow-Origin': 'https://tracker.gg',
+		},
+	}
+);
